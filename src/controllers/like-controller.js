7ff76cb35edf46a9d451cd93cb76d4e71bffef10
Aman Pratap Singh <amanpratapsingh030@gmail.com>
2024-08-02 21:28:21 +0530

    Added Toggle like controller and Toggle like Api route.

---
+++ src/controllers/like-controller.js
@@ -0,0 +1,32 @@
+import { LikeService } from "../services/index.js";
+
+const likeService = new LikeService();
+
+const toggleLike = async(req, res) => {
+    try {
+        const modelId = req.query.modelId;
+        const modelType = req.query.modelType;
+        const userId = req.body.userId;
+
+        const islike = await likeService.toggleLike(modelId,modelType,userId);
+        
+        res.status(200).json({
+            success: true,
+            isLiked: islike,
+            message: "Toggle Like Operation done.",
+            err: {} 
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            data: {},
+            message: "Something went wrong in Toggle Like Operation.",
+            err: error 
+        });
+    }
+}
+
+export default {
+    toggleLike
+}
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,7 +2,9 @@ import express from 'express';
 const router = express.Router();
 
 import TweetController from '../../controllers/tweet-controller.js';
+import LikeController from '../../controllers/like-controller.js';
 
 router.post('/tweets', TweetController.createTweet);
+router.post('/likes/toggle', LikeController.toggleLike);
 
 export default router;
